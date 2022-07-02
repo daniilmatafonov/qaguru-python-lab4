ddcc679b3f41b01aa4c4eb5961b750c0be5a3c53
Daniil Matafonov <danielmt.dm@gmail.com>
2022-07-02 13:20:35 +0300

    add print function, implement call

--- homework.py
+++ homework.py
@@ -3,13 +3,26 @@
 # Вызовите ее внутри функций, описанных ниже
 # Подсказка: Имя функции можно получить с помощью func.__name__
 
-def open_browser(browser_name):
+
+def open_browser(name):
+    print_name(open_browser, name)
     pass
 
 
-def go_to_companyname_homepage(page_url):
+def go_to_companyname_homepage(homepage):
+    print_name(go_to_companyname_homepage, homepage)
     pass
 
 
 def find_registration_button_on_login_page(page_url, button_text):
+    print_name(find_registration_button_on_login_page, page_url, button_text)
     pass
+
+
+def print_name(f_name, *args):
+    print("Func name: ", f_name.__name__.replace('_', ' ').capitalize(), "args: ", *args)
+
+
+open_browser('chrome')
+go_to_companyname_homepage('https://demoqa.com/automation-practice-form')
+find_registration_button_on_login_page('https://demoqa.com/automation-practice-form', 'Submit')
